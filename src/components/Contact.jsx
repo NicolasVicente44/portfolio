26971276d944b7a6e3421b1530a198af65c239cb
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-06-30 19:59:22 -0400

    Added forma submission functionality via getforms.io

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#7e3431]  flex justify-center items-center p-4'>
-        <form method='POST' action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/0d9f6adb-6c59-4ee6-a292-cde55b87e166" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#000000] text-gray-300'>Contact Me</p>
                 <p className='text-gray-300 py-4'>Submit the form below or reach me by email - nicolas.a.vicente44@gmail.com</p>
